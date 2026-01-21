11b64aa812fc2fc5ecdf5b7f6f7435de178efcb6
SANDHILLS\LH218-15 <jad.talbert@gmail.com>
2026-01-21 11:37:33 -0500

    my first Java program

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,16 @@
+/*
+Author: Landon Goggans
+Date: 1/21/2026
+@purpose: My First Java Program
 
+Note: This  program should reside in the directory path as follows->    CSC_151_LANDON_GOGGANS
+*/
+
+
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+      System.out.println("Hello World");
+    }
+}
