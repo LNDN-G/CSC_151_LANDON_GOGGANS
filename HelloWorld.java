025d48c7f3dd6c80adf591c99c4adb6084b4f45c
SANDHILLS\LH218-15 <jad.talbert@gmail.com>
2026-01-28 10:07:17 -0500

    added at sign

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 /*
-Author: Landon Goggans
-Date: 1/21/2026
+@Author: Landon Goggans
+@Date: 1/21/2026
 @purpose: My First Java Program
 
 Note: This  program should reside in the directory path as follows->    CSC_151_LANDON_GOGGANS
