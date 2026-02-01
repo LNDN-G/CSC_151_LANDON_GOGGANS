eef7c342f2f3bf188bcbe9657e1321104a77705b
LNDN-G <gogganslg145@mail.sandhills.edu>
2026-02-01 17:27:23 -0500

    Addded mammal files

---
+++ src/labs/example/mammals/Mammal.java
@@ -0,0 +1,64 @@
+/*
+@Author: Landon Goggans
+@Date: 2/1/2026
+@Purpose: To show off my mammal's properties
+*/
+
+package labs.example.mammal;
+
+public class Mammal
+{
+    //Properties
+    private String hairColor;
+    private String eyeColor;
+    private double bodyTemp;
+    private int lifespan;
+    private double weight;
+    private double height;
+
+    //Methods
+    public void run()
+    {
+        System.out.println("The mammal can run.");
+    }
+
+    public void eat()
+    {
+        System.out.println("The mammal does eat.");
+    }
+
+    public void sleep()
+    {
+        System.out.println("The mammal goes to sleep at night.");
+    }
+
+    public void scratch()
+    {
+        System.out.println("The mammal can scratch.");
+    }
+
+    public void fight()
+    {
+        System.out.println("The mammal can fight for itself.");
+    }
+
+    public void drink()
+    {
+        System.out.println("The mammal can drink.");
+    }
+
+    public void breathe()
+    {
+        System.out.println("The mammal does breathe.");
+    }
+
+    public void move()
+    {
+        System.out.println("The mammal can move around.");
+    }
+
+    public void reproduce()
+    {
+        System.out.println("The mammal can reproduce.");
+    }
+}
