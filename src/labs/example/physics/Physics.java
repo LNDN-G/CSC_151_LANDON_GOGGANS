972edcda5968a8f315e08b16387fdefecbd1e02f
SANDHILLS\LH218-15 <jad.talbert@gmail.com>
2026-02-09 11:46:58 -0500

    Added Physics

---
+++ src/labs/example/physics/Physics.java
@@ -0,0 +1,15 @@
+/*
+@Author: Landon Goggans
+@Date: 2/9/2026
+@Purpose: To show off Physics
+*/
+
+package labs.example.physics;
+import java.lang.Math;
+
+public class Physics{
+
+    final static double GRAVITY = 9.81;
+
+    
+}
