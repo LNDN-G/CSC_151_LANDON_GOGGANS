ac606fc64bc6470268095c842afc4000591e96cc
LNDN-G <gogganslg145@mail.sandhills.edu>
2026-02-22 21:40:02 -0500

    Ready for Lab upload

--- src/labs/example/physics/Main.java
+++ src/labs/example/physics/Main.java
@@ -1,7 +1,7 @@
 /*
 @Author: Landon Goggans
 @Date: 2/11/2026
-@Purpose: Main class file for physics
+@Purpose: Main class is used to test Physics class
 */
 
 package labs.example.physics;
@@ -11,21 +11,51 @@ public class Main
 {
     public static void main(String[] args)
     {
+        //Creates a Physics object
         Physics physics = new Physics();
 
-        double distance = physics.getDistance(15, 4);
-        System.out.println("Distance: " + distance + " miles");
+        //Calculates velocity
+        double velocity1 = physics.getVelocity(80, 2);
+        System.out.println("Velocity: " + velocity1 + " mph");
 
-        double velocity = physics.getVelocity(80, 2);
-        System.out.println("Velocity: " + velocity + " mph");
-
+        //Calculates momentum
         double momentum = physics.getMomentum(500, 60);
         System.out.println("Momentum: " + momentum + " kg m/s");
 
+        //Calculates force
         double force = physics.getForce(12, 4);
         System.out.println("Force: " + force + " kg m/s^2");
 
+        //Calculates work
         double work = physics.getWork(20, 10);
         System.out.println("Work: " + work + " Joules");
+
+        //Calculates angle
+        double angle = physics.getAngle(3, 4);
+        System.out.println("Angle: " + angle + " degrees");
+
+        //Conditional to check if the angle is a 3-4-5 triangle
+        if (angle > 37 || angle < 36.87) {
+            physics.logInvalidAngleInfo(angle);
+        } else {
+            physics.logValidAngleInfo(angle);
+        }
+
+        //Light speed and time calculation
+        double velocity = physics.getLightSpeedInMPH();
+        double time = physics.getTimeFromSunToEarthInHours();
+
+        //Calculates distance with getDistance method
+        double distance = physics.getDistance(velocity, time);
+
+        //Known distance from Sun to Earth
+        double knownDistance = physics.getKnownDistanceToEarth();
+
+        //Conditional to check if the distance is correct
+        if (distance != knownDistance){
+            physics.logEarthToSunInvalidDistance(distance);
+        } else {
+            System.out.println("The distance from the Sun to Earth is valid at " + distance + " miles.");
+        }
     }
 }
--- src/labs/example/physics/Physics.java
+++ src/labs/example/physics/Physics.java
@@ -1,7 +1,7 @@
 /*
 @Author: Landon Goggans
 @Date: 2/9/2026
-@Purpose: To show off Physics
+@Purpose: To show off Physics equations and calculations
 */
 
 package labs.example.physics;
@@ -9,43 +9,72 @@ import java.lang.Math;
 
 public class Physics{
 
+    //Value for gravity (m/s^2)
     final static double GRAVITY = 9.81;
 
+    //Calculates distance using x = v * t
     public double getDistance(double v, double t){
         double x = v * t;
         return x;
     }
 
+    //Calculates velocity using v = x / t
     public double getVelocity(double x, double t){
         double v = x / t;
         return v;
     }
 
+    //Calculates momentum using p = m * v
     public double getMomentum(double m, double v){
         double p = m * v;
         return p;
     }
 
+    //Calculates force using f = m * a
     public double getForce(double m, double a){
         double f = m * a;
         return f;
     }
 
+    //Calculates work using w = f * d
     public double getWork(double f, double d){
         double w = f * d;
         return w;
     }
 
-    public static double getTheta(int xVal,int yVal){
-        double theta = Math.atan2(xVal,yVal) * 180/3.1415;
-        return theta;
+    //Calculates angle using atan2
+    public double getAngle(double opposite, double adjacent){
+        return Math.toDegrees(Math.atan2(opposite, adjacent));
     }
 
-    public double logInvalidAngleInfo(){
-        Sysyem.out.println("logging the angle xy degrees. This is not a right angle.");
+    //Logs invalid angle information
+    public void logInvalidAngleInfo(double angle){
+        System.out.println("logging the angle " + angle + " degrees. This is not a right angle.");
     }
 
-    public double logValidAngleInfo(){
-        System.out.println("logging the angle xy degrees. This is a valid 3-4-5 triangle.");
+    //Logs valid angle information
+    public void logValidAngleInfo(double angle){
+        System.out.println("logging the angle " + angle + " degrees. This is a valid 3-4-5 triangle.");
+    }
+
+    //Calculates speed of light in miles per hour
+    public double getLightSpeedInMPH(){
+        return 186282 * 3600; // 186282 miles per second * 3600 seconds per hour
+    }
+
+    //Calculates time from Sun to Earth in hours
+    public double getTimeFromSunToEarthInHours(){
+        double distanceToEarth = getKnownDistanceToEarth();
+        return distanceToEarth / getLightSpeedInMPH();
+    }
+
+    //Logs invalid distance from Earth to Sun
+    public void logEarthToSunInvalidDistance(double new_distance){
+        System.out.println("logging the distance " + new_distance + " miles. This is not the correct distance from the Earth to the Sun.");
+    }
+
+    //Returns known distance from the Earth to the Sun
+    public double getKnownDistanceToEarth(){
+        return 92947266.72;
     }
 }
