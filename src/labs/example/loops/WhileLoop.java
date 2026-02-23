b3efb1819c50d122c0c28f03d5ba233e2b866648
SANDHILLS\LH218-15 <jad.talbert@gmail.com>
2026-02-23 11:42:36 -0500

    Added loops folder

---
+++ src/labs/example/loops/WhileLoop.java
@@ -0,0 +1,22 @@
+/*
+@Author: Landon Goggans
+@Date: 2/23/2026
+@Purpose: 
+*/
+
+package labs.example.loops;
+
+public class WhileLoop
+{
+    private void executeWhileLoop()
+    {
+        int number = 0;
+
+        while (number <= 10)
+        {
+            System.out.println("Number of Iterations: " + {number})
+        }
+
+        System.out.println("The loop is now complete.")
+    }
+}
