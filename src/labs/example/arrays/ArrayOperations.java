de952599b9c10734e5bf21947753f913c42b3ccb
LNDN-G <gogganslg145@mail.sandhills.edu>
2026-03-08 23:39:11 -0400

    Ready for lab

--- src/labs/example/arrays/ArrayOperations.java
+++ src/labs/example/arrays/ArrayOperations.java
@@ -8,33 +8,40 @@ package labs.example.arrays;
 
 public class ArrayOperations{
 
+    //Creates a new array 
     public void createNewArray(int size) {
 
+        //Creates a new array of integers
         int[] newArray = new int[size];
 
+        //Uses a for loop to populate the array with values based on the index
         for (int i = 0; i < newArray.length; i++) {
             newArray[i] += i;
         }
 
         System.out.println("I created a new array and it now has " + newArray.length + " items in it.");
-
         System.out.println("The array items and their values are listed below:");
 
+        //Calls the displayArray method to print the items in the array
         displayArray(newArray);
     }
 
+    //Displays the items in the array
     private void displayArray(int[] array) {
 
+        //Uses a for loop to print the index and value of each item in the array
         for (int i = 0; i < array.length; i++) {
             System.out.println("Index " + i + " = " + array[i]);
         }
     }
 
+    //Sorts the array in ascending order
     public void sortArray(int[] array) {
 
         int n = array.length;
         int temp;
 
+        //Uses bubble sort method to sort array
         for (int i = 0; i < n - 1; i++) {
 
             for (int j = 0; j < n - 1 - i; j++) {
@@ -50,11 +57,13 @@ public class ArrayOperations{
 
         System.out.println("The sorted array is:");
 
+        //Uses a for loop to print the sorted array
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i] + " ");
         }
     }
 
+    //Displays the number of days in each month
     public void getDaysAndMonths() {
 
         int[] days = {
@@ -66,6 +75,7 @@ public class ArrayOperations{
             "July", "August", "September", "October", "November", "December"
         };
 
+        //Uses a for loop to print the number of days in each month
         for (int i = 0; i < months.length; i++) {
             System.out.println("There are " + days[i] + " days in " + months[i]);
         }
--- src/labs/example/arrays/Main.java
+++ src/labs/example/arrays/Main.java
@@ -10,10 +10,13 @@ public class Main
 {
     public static void main(String[] args)
     {
+        //Creates a new ArrayOperations class to call its methods
         ArrayOperations arrayOps = new ArrayOperations();
 
+        //Calls the createNewArray method to create a new array of size 10
         arrayOps.createNewArray(10);
 
+        //Array of 50 random unsorted integers
         int[] unsortedArray = {
             58, 43, 10, 47, 64, 19, 82, 31, 74, 5, 
             99, 25, 60, 8, 93, 14, 70, 37, 88, 21, 
@@ -22,8 +25,10 @@ public class Main
             11, 89, 67, 23, 56, 34, 90, 3, 78, 12
         };
 
+        //Calls the sortArray method to sort the unsorted array
         arrayOps.sortArray(unsortedArray);
 
+        //Calls the getDaysAndMonths method to print the number of days in each month
         arrayOps.getDaysAndMonths();
     }
 }
